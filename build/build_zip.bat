bc074aaec482b22c9294b7ded4390ecec42dda2e
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2013-12-30 22:40:54 +0100

    build utils

--- build/build_zip.bat
+++ build/build_zip.bat
@@ -5,4 +5,4 @@ set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:
 echo %datetimef%
 SET location=%CD%
 
-call %location%\..\bin\7zip\7za.exe a -tzip %location%\..\ctree_%datetimef%.zip %location%\..\app\* %location%\..\phonegap\* -x!*node_modules -x!*services -x!app.js -x!*.bat -x!*.7z -x!*.log -x!favicon.ico -x!favicon.png -x!package.json
+call %location%\..\bin\7zip\7za.exe a -tzip %location%\..\inercia_%datetimef%.zip %location%\..\app\* %location%\..\phonegap\* -x!*node_modules -x!*services -x!app.js -x!*.bat -x!*.7z -x!*.log -x!favicon.ico -x!favicon.png -x!package.json
--- build/build_zip.sh
+++ build/build_zip.sh
@@ -5,5 +5,5 @@ echo 'Compression started'
 datetimef=$(date +"%Y_%m_%d__%H_%M_%S")
 location=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-7za a -tzip $location/ctree_$datetimef.zip $location/app/* $location/phonegap/* -x!*node_modules -x!*services -x!app.js -x!*.bat -x!*.7z -x!*.log -x!favicon.ico -x!favicon.png -x!package.json
+7za a -tzip $location/inercia_$datetimef.zip $location/app/* $location/phonegap/* -x!*node_modules -x!*services -x!app.js -x!*.bat -x!*.7z -x!*.log -x!favicon.ico -x!favicon.png -x!package.json
 echo 'Compression finished'
